e2e498af3c3fc4116b2d108e3322b52e5b6f0862
Furkan <furkankoc18@gmail.com>
2020-11-10 21:05:38 +0300

    second commit

--- src/main/java/com/springmvc/SpringBootCrudApplication.java
+++ src/main/java/com/springmvc/SpringBootCrudApplication.java
@@ -2,6 +2,7 @@ package com.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//furkan koc
 
 @SpringBootApplication
 public class SpringBootCrudApplication {
